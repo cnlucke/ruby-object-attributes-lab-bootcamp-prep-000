a2a82a2b11dd00b0eb9bb139cd746edf0c6094aa
Cristy Lucke <cristy.lucke@gmail.com>
2017-10-17 15:14:26 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,12 +1,12 @@
 class Person
-  def name=(name)
-    @name = name
+  def name=(new_name)
+    @name = new_name
   end
   def name
     puts @name
   end
-  def job=(job)
-    @job = job
+  def job=(job_name)
+    @job = job_name
   end
   def job
     puts @job
