ed1b73ee0e7eb440bac1511f72cb76e0162c74da
Cristy Lucke <cristy.lucke@gmail.com>
2017-10-17 15:14:49 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,12 @@
 class Dog
-  def name=(name)
-    @name = name
+  def name=(new_name)
+    @name = new_name
   end
   def name
     puts @name
   end
-  def breed=(breed)
-    @breed = breed
+  def breed=(breed_name)
+    @breed = breed_name
   end
   def breed
     puts @breed
